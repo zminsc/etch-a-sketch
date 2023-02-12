007741055335fee25b86c0aa9eae26864c789dd9
Steven Chang <zminsc@seas.upenn.edu>
2023-02-12 12:17:51 -0500

    Improve event handler efficiency
    Remove mousedown, mousemove event handlers from grid cells upon drawn

--- draw.js
+++ draw.js
@@ -1,20 +1,32 @@
 let mouseClicked = false;
 
+function onDrawn(event) {
+    event.currentTarget.removeEventListener("mousedown", handleMouseDown);
+    event.currentTarget.removeEventListener("mousemove", handleMouseMove);
+    event.currentTarget.addEventListener("mousedown", () => mouseClicked = true);
+}
+
+function handleMouseDown(event) {
+    mouseClicked = true;
+    event.currentTarget.classList.add("drawn");
+    onDrawn(event);
+}
+
+function handleMouseMove(event) {
+    if (mouseClicked) {
+        event.currentTarget.classList.add("drawn");
+        onDrawn(event);
+    }
+}
+
+function handleMouseUp() {
+    mouseClicked = false;
+}
+
 const gridCells = document.querySelectorAll(".grid-cell");
 
 for (const gridCell of gridCells) {
-    gridCell.addEventListener("mousedown", () => {
-        mouseClicked = true,
-        gridCell.classList.add("drawn")
-    });
-
-    gridCell.addEventListener("mousemove", () => {
-        if (mouseClicked) {
-            gridCell.classList.add("drawn");
-        }
-    });
-
-    gridCell.addEventListener("mouseup", () =>
-        mouseClicked = false
-    );
+    gridCell.addEventListener("mousedown", handleMouseDown);
+    gridCell.addEventListener("mousemove", handleMouseMove);
+    gridCell.addEventListener("mouseup", handleMouseUp);
 }
